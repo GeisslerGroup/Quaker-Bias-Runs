d6db3cf19292df93e8e6ccf655da1cbcaf85b353
Pratima Satish <pratimasatish@berkeley.edu>
2017-02-07 18:08:55 -0800

    update

--- phase-colour.sh
+++ phase-colour.sh
@@ -1,5 +1,11 @@
 #!/bin/bash --login
 
-python less_frame.py -bias 0.65 -step 500 > sparse0.65.xyz
-./strip_dump.sh sparse0.65.xyz sparse0.65.stripped
-python colour_phase.py -f sparse0.65.stripped -div 0.62 > colour0.65.xyz
+biaslist=$(seq 0.35 0.05 0.95)
+
+for b in ${biaslist}
+do
+    python less_frame.py -bias ${b} -step 500 > sparse${b}.xyz
+    ./strip_dump.sh sparse${b}.xyz sparse${b}.stripped
+    python colour_phase.py -f sparse${b}.stripped -div 0.8 > colour${b}.xyz
+    mv sparse${b}.xyz sparse${b}.stripped colour${b}.xyz sparse-files/
+done
