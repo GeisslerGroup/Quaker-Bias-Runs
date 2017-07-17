67b67d0a658b29920facd04af595f2d38c0c8676
Pratima Satish <pratimasatish@berkeley.edu>
2017-07-17 15:59:03 -0700

    colouring trajectories in two different ways

---
+++ 350.18K/phase-colour.sh
@@ -0,0 +1,12 @@
+#!/bin/bash --login
+
+biaslist=$(seq $1 0.025 $2)
+
+for b in ${biaslist}
+do
+    python less_frame.py -bias ${b} -step 50 > sparse${b}.xyz
+    ./strip_dump.sh sparse${b}.xyz stripped${b}
+#     python colour_phase.py -f stripped${b} -div 0.77777778 > anti-gauche${b}.xyz
+    python colour_phase.py -f stripped${b} -div 0.575 > O-DO${b}.xyz
+    rm stripped${b} sparse${b}.xyz
+done
