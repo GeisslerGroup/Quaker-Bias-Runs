58d9575bdf98c449c179a0cb6c6e2930a08c8009
Pratima Satish <pratimasatish@berkeley.edu>
2017-08-02 18:50:28 -0700

    automating dump merging process

--- last_time.sh
+++ last_time.sh
@@ -1,4 +1,6 @@
 #!/bin/bash --login
 
 STRING=$(tail -n 22117 $1 | head -n 1)
-echo $STRING | awk -v N=$3 '{print $3}'
+TIME=$(echo $STRING | awk -v N=$3 '{print $3}')
+
+python merge_dump.py -time $TIME -fil $1 -append $2 > dump-soft-0.7250.xyz
